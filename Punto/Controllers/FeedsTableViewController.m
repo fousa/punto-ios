c1958c842da84defba6da69cc841d3978f6da4fc
Jelle Vandebeeck <jelle@fousa.be>
2014-04-15 21:53:33 +0200

    Delete feed from the list.

--- Punto/Controllers/FeedsTableViewController.m
+++ Punto/Controllers/FeedsTableViewController.m
@@ -70,7 +70,13 @@
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        NSLog(@"remove path %@", indexPath);
+        Feed *feed = _feeds[indexPath.row];
+        [MagicalRecord saveWithBlockAndWait:^(NSManagedObjectContext *localContext) {
+            Feed *localFeed = [feed MR_inContext:localContext];
+            [localFeed MR_deleteInContext:localContext];
+        }];
+        [_feeds removeObjectAtIndex:indexPath.row];
+        [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
     }
 }
 
