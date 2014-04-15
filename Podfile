dcda94ab1aa96856b7fc477942086ac6279af1aa
Jelle Vandebeeck <jelle@fousa.be>
2014-04-15 15:35:19 +0200

    Show map.

--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 platform :ios, '7.0'
 
 pod 'coby', '~> 0.0.1'
+pod 'Masonry', '~> 0.4'
--- Punto/Controllers/MapViewController.m
+++ Punto/Controllers/MapViewController.m
@@ -6,17 +6,27 @@
 //  Copyright (c) 2014 Fousa. All rights reserved.
 //
 
+#import <MapKit/MapKit.h>
+
 #import "MapViewController.h"
 
 @interface MapViewController ()
 @end
 
-@implementation MapViewController
+@implementation MapViewController {
+    MKMapView *_mapView;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor greenColor];
+    _mapView = [MKMapView new];
+    [self.view addSubview:_mapView];
+    
+    __weak MapViewController *weakSelf = self;
+    [_mapView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(weakSelf.view).with.insets((UIEdgeInsets) { 0, 0, 0, 0 });
+    }];
 }
 
 @end
